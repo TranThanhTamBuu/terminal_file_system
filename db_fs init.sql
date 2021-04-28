187a241ffe4a1d819232b73a561f5ecf6a0a66df
TranThanhTamBuu <18127268@student.hcmus.edu.vn>
2021-04-29 02:17:44 +0700

    try to deploy but fail

---
+++ db_fs init.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE db_fs;
+
+USE DATABASE db_fs;
+
+CREATE TABLE path
+(
+	partition_key CHAR(64) NOT NULL PRIMARY KEY,
+	folder_name VARCHAR(255),
+	bytes BIGINT,
+	PRIMARY KEY(partition_key, folder_name)
+);
+
+CREATE TABLE root
+(
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(255) NOT NULL,
+	create_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	data TEXT,
+	bytes BIGINT
+);
+
+INSERT INTO path VALUES ("root", "root", 0)
